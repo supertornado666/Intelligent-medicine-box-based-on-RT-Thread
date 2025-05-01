5df591a55442bd22e44c4c5e1ebe887236a016f2
supertornado666 <91970546+supertornado666@users.noreply.github.com>
2025-05-01 15:12:52 +0800

    修改OpenMV逻辑

--- OpenMV/find_barcode.py
+++ OpenMV/find_barcode.py
@@ -1,11 +1,11 @@
 # 条形码识别例程
 #
 # 这个例子展示了使用OpenMV Cam M7来检测条形码是多么容易。条形码检测不适用于M4相机。
-START_SCAN = 'I'
-FINISH_SCAN = 'J'
+START_SCAN = b'J'
+FINISH_SCAN = b'K'
 
 import sensor, image, time, math
-from pyb import UART
+from pyb import UART, ExtInt
 import ustruct
 import pyb
 
@@ -19,6 +19,16 @@ clock = time.clock()
 
 uart = UART(3,115200,bits=8, parity=None, stop=1, timeout_char = 1000)#初始化串口三、波特率115200 TXD:P4\PB10 RXD:P5\PB11
 
+flag = False  # 全局标志位，表示是否需要启动扫描
+
+# 中断回调函数：设置标志位
+def uart_rx_callback(line):
+    global flag
+    flag = True  # 被唤醒，准备处理数据
+
+# 初始化 UART RX 引脚为中断源（P5 对应引脚编号为 'P5'）
+extint = ExtInt('P5', ExtInt.IRQ_FALLING, pyb.Pin.PULL_UP, uart_rx_callback)
+
 def send_codenum(codenum):#功能发送五个无符号字符（unsigned char）
     global uart;
     #data = ustruct.pack("<B", codenum)
@@ -27,6 +37,7 @@ def send_codenum(codenum):#功能发送五个无符号字符（unsigned char）
     print(codenum)#通过 print(data) 打印发送的数据到串行终端，方便调试和确认发送的内容。
 
 def start_scan():
+    global flag
     sensor.skip_frames(time = 2000)
 
     while True:
@@ -37,23 +48,20 @@ def start_scan():
             #img.draw_rectangle(code.rect())
             send_codenum(codes[0].payload())
             break
-        if uart.any():
-            command = uart.read()
-            if command == FINISH_SCAN:
-                break
+        if flag:
+            pyb.delay(100)
+            flag = False
+            break
 
 # 条形码检测可以在OpenMV Cam的OV7725相机模块的640x480分辨率下运行。
 # 条码检测也将在RGB565模式下工作，但分辨率较低。 也就是说，
 # 条形码检测需要更高的分辨率才能正常工作，因此应始终以640x480的灰度运行。
 
 while True:
-    if uart.any():
-        command = uart.read()
-        if command == START_SCAN:
-            command = ""
-            start_scan()
+    if flag:
+        pyb.delay(100)
+        flag = False
 
-        time.sleep_ms(100)
-        continue
+        start_scan()
 
     pyb.stop()
